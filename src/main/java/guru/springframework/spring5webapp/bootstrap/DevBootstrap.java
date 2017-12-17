4a225f85d9828a1ce2603bd9d9f4c3edbb42dc14
pradithya aria <pradithya.aria@gmail.com>
2017-12-17 12:03:40 +0800

    Add publisher entity

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,42 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.model.repository.AuthorRepository;
+import guru.springframework.spring5webapp.model.repository.BookRepository;
+import guru.springframework.spring5webapp.model.repository.PublisherRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+        Author eric = new Author("Eric", "Demaine");
+        Publisher harperCollins = new Publisher("Harper Collins", "USA");
+        Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        publisherRepository.save(harperCollins);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+    }
+}
